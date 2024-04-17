5578195d83792f383b421b382a2ef3917613710f
Daniel Mozsar <jezzy@pop-os.t.hu>
2024-04-17 15:39:20 +0200

    Made toasts to be stackable

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -3,9 +3,11 @@
 
     <div class="fixed bottom-0 right-0 w-full sm:w-auto px-3">
     <Toast
-      v-if="$page.props.flash.message"
-      :message="$page.props.flash.message.message"
-      :type="$page.props.flash.message.type"
+      v-if="messages.length > 0"
+      v-for="(message, index) in messages"
+        :key="index"
+        :message="message.message"
+        :type="message.type"
     />
   </div>
     <slot></slot>
@@ -14,4 +16,30 @@
 <script setup>
 import PrimaryMenu from "@/Components/PrimaryMenu.vue";
 import Toast from "@/Components/Toast.vue";
+import { ref, watch, onMounted } from "vue";
+import { usePage } from "@inertiajs/vue3";
+
+// TODO: Refactor toast logic into a BaseComponent
+
+// Get global props
+const page = usePage();
+
+// State
+const messages = ref([]);
+
+
+// Check for message onMounted
+onMounted(() => {
+  if(page.props.flash.message) {
+    messages.value.push(page.props.flash.message);
+  }
+})
+
+// Check for messages so we can stack them
+watch(() => page.props.flash.message, (newValue) => {
+  if(newValue) {
+    messages.value.push(newValue);
+  }
+})
+
 </script>
--- resources/js/Pages/Account/Create.vue
+++ resources/js/Pages/Account/Create.vue
@@ -196,10 +196,12 @@
         ></div>
         <div class="fixed bottom-0 right-0 w-full sm:w-auto px-3">
             <Toast
-            v-if="$page.props.flash.message"
-            :message="$page.props.flash.message.message"
-            :type="$page.props.flash.message.type"
-        />
+      v-if="messages.length > 0"
+      v-for="(message, index) in messages"
+        :key="index"
+        :message="message.message"
+        :type="message.type"
+    />
   </div>
     </div>
 </template>
@@ -216,6 +218,31 @@ import { Link } from "@inertiajs/vue3";
 import { useForm } from "@inertiajs/vue3";
 import Dropdown from "primevue/dropdown";
 import Toast from "@/Components/Toast.vue";
+import { ref, watch, onMounted } from "vue";
+import { usePage } from "@inertiajs/vue3";
+
+// TODO: Refactor toast logic into a BaseComponent
+
+// Get global props
+const page = usePage();
+
+// State
+const messages = ref([]);
+
+// Check for message onMounted
+onMounted(() => {
+  if(page.props.flash.message) {
+    messages.value.push(page.props.flash.message);
+  }
+})
+
+// Check for messages so we can stack them
+watch(() => page.props.flash.message, (newValue) => {
+  if(newValue) {
+    messages.value.push(newValue);
+  }
+})
+
 
 // Init form
 const form = useForm({
