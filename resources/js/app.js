828d2a1419d180b8bbd43d33dd867dbe3d7fe761
Daniel Mozsar <jezzy@pop-os.t.hu>
2024-04-20 22:48:12 +0200

    Done

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,6 +9,17 @@ import "primevue/resources/themes/md-light-indigo/theme.css";
 import MainLayout from "./Layouts/MainLayout.vue";
 
 createInertiaApp({
+
+    // Progress indicator
+    progress: {
+        delay: 250, // Only show bar if request takes more than 250ms
+        color: '#3F51B5',
+        includeCSS: true,
+        showSpinner: true
+    },
+
+
+    // Resolve vue files, layouts
     resolve: (name) => {
         const pages = import.meta.glob("./Pages/**/*.vue", { eager: true });
 
