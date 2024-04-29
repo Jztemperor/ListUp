d742424177737d3acd80be34c9185d1e78a6f8d0
Jztemperor <daniel.mozsar01@gmail.com>
2024-04-29 21:32:09 +0200

    Email verification works, needs to be refactored into a controller

---
+++ resources/js/Pages/Verification/verify-email.vue
@@ -0,0 +1,60 @@
+<template>
+    <h1 class="gap-4 my-10 text-3xl text-center lg:text-4xl xl:text-5xl text-brand-color">Verify your account!</h1>
+    <p class="p-2 my-5 text-center xl:p-0">Thank you for creating an account with us! <span class="font-bold text-brand-color">Verification steps:</span></p>
+    
+    <ol class="flex flex-col items-center justify-center w-full p-5 xl:p-0">
+        <div>
+            <li class="mb-3">
+            <span class="font-bold text-brand-color">1. Check Your Email:</span> 
+            A verification link has been sent to the email address you provided during registration.</li>
+
+        <li class="mb-3">
+            <span class="font-bold text-brand-color">2. Open Your Inbox:</span> 
+            Go to your email inbox and look for an email from us. If you don’t see it in your inbox, please check your spam or junk folder.
+        </li>
+
+        <li class="mb-3">
+            <span class="font-bold text-brand-color">
+                3. Click the Verification Link:
+            </span>
+            In the email, you will find a link to verify your account. Click on the provided link to complete the verification process.
+        </li class="mb-3">
+
+        <li class="mb-3">
+            <span class="font-bold text-brand-color">
+               4. Confirmation:
+            </span>
+            Once you click the link, you will be redirected to our website, and your account will be successfully verified.
+        </li>
+
+        <li class="mb-3">
+            <span class="font-bold text-brand-color">
+               Did not receive our mail?
+            </span>
+           <form @submit.prevent="form.post(route('verification.send'))" class="inline">
+                <button type="submit" class="ml-2 underline">Resend Email Verification</button>
+            </form>
+        </li>
+
+        </div>
+    </ol>
+
+
+    <div class="mt-3 text-center ">
+        <p class="mb-5">
+            <span class="font-bold text-brand-color">Note:</span> 
+            If you encounter any issues or did not receive the verification email, please contact our support team at <span class="font-bold text-brand-color">support@listup.com</span> for assistance.
+        </p>
+
+        <p>Thank you for choosing our platform. We look forward to providing you with a great experience!</p>
+    </div>
+
+</template>
+
+<script setup>
+import { useForm } from "@inertiajs/vue3";
+
+const form = useForm({});
+
+
+</script>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\AccountController;
 use App\Http\Controllers\AuthenticationController;
 use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,3 +29,33 @@ Route::resource('account', AccountController::class)
 Route::get('login', [AuthenticationController::class, 'create'])->name('login');
 Route::post('login', [AuthenticationController::class, 'store'])->name('login');
 Route::delete('logout', [AuthenticationController::class, 'destroy'])->name('logout');
+
+// Email verification view
+Route::get('/email/verify', function () {
+  return inertia('Verification/verify-email');
+})->middleware('auth')->name('verification.notice');
+ 
+
+// Fulfill verification
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+ 
+    return redirect('/')->with([
+            'message' => [
+                'type' => 'success',
+                'message' => "You're email has been verified!"
+            ]
+        ]);
+})->middleware(['auth', 'signed'])->name('verification.verify');
+ 
+// Re-send verification link
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+ 
+    return back()->with([
+            'message' => [
+                'type' => 'success',
+                'message' => "Verification email sent!"
+            ]
+        ]);
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
